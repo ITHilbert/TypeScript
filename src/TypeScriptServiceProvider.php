40357bd992bf4f1a5f3a182bab358f4b2c93a17d
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2021-07-02 13:43:18 +0200

    neue JS

--- src/TypeScriptServiceProvider.php
+++ src/TypeScriptServiceProvider.php
@@ -14,18 +14,25 @@ class TypeScriptServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Vue Compontents kopieren nach Public
-        $this->publishes([
-            __DIR__ .'/assets' => public_path('vendor/typescript'),
-        ]);
+      //JavaScript kopieren nach Public
+      $this->publishes([
+        __DIR__ .'/js' => public_path('vendor/typescript'),
+      ]);
 
-        //Ressources js und sass
-        $this->publishes([
-            __DIR__.'/ts' => resource_path('ts'),
-        ]);
+      //TypeScripts kopieren nach Public
+      $this->publishes([
+          __DIR__ .'/assets' => public_path('vendor/typescript'),
+      ]);
 
 
-        //Commands Registrieren
-        $this->commands( \ITHilbert\TypeScript\App\Console\Commands\TypeScriptCopyFiles::class );
+
+      //Ressources js und sass
+      $this->publishes([
+          __DIR__.'/ts' => resource_path('ts'),
+      ]);
+
+
+      //Commands Registrieren
+      $this->commands( \ITHilbert\TypeScript\App\Console\Commands\TypeScriptCopyFiles::class );
     }
 }
